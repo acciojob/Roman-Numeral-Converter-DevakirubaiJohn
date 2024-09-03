0237e8ae673592f72ab2dd56dcc08912687fbacc
DevakirubaiJohn <johndevakirubai21@gmail.com>
2024-09-03 22:05:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    const obj = {
+        0:['M',1000], 
+        1:['D', 500], 
+        2:['C', 100], 
+        3:['L', 50], 
+        4:['X', 10], 
+        5:['V', 5], 
+        6:['I', 1]
     };
 
-  //your code here
-	  let result = "";
+    let result = "";
 
     // iterate over the object in reverse order
     for(let i = 6; i >= 0; i--) {
@@ -26,7 +25,6 @@ function convertToRoman(num) {
 
     // return the result
     return result;
-
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
