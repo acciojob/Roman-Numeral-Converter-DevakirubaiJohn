790cb123a58b99bbf0fda55bb05275b245ac6661
DevakirubaiJohn <johndevakirubai21@gmail.com>
2024-09-03 21:48:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ function convertToRoman(num) {
     };
 
   //your code here
+	  let result = "";
+
+    // iterate over the object in reverse order
+    for(let i = 6; i >= 0; i--) {
+        // calculate how many times the current value can be subtracted from num
+        let times = Math.floor(num / obj[i][1]);
+
+        // append the corresponding numeral to result as many times as the value was subtracted
+        result += obj[i][0].repeat(times);
+
+        // update num by getting the remainder of the division
+        num %= obj[i][1];
+    }
+
+    // return the result
+    return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
